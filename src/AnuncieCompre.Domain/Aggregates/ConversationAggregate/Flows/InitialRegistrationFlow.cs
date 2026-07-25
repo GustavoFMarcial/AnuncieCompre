cba9e65a4f2fcd7451845d7ab3caad8a9ec5f03e
GustavoFMarcial <gfm211101@gmail.com>
2026-07-25 10:42:57 -0400

    Refatorar InitiralRegistrationFlow

--- src/AnuncieCompre.Domain/Aggregates/ConversationAggregate/Flows/InitialRegistrationFlow.cs
+++ src/AnuncieCompre.Domain/Aggregates/ConversationAggregate/Flows/InitialRegistrationFlow.cs
@@ -11,19 +11,29 @@ public class InitialRegistrationFlow
 {
     public static IReadOnlyDictionary<string, IConversationNode> Build()
     {
-        IValueObjectValidator userTypeValidator = new UserTypeValidator();
         IValueObjectValidator emailValidator = new EmailValidator();
         IValueObjectValidator nameValidator = new NameValidator();
+        IValueObjectValidator quantityValidator = new QuantityValidator();
+        IValueObjectValidator productValidator = new ProductValidator();
+        IValueObjectValidator companyCategoryValidator = new CompanyCategoryValidator();
 
-        INodeValidator askUserTypeValidator = new OptionValidationNodeValidator(["1", "2"], userTypeValidator);
         INodeValidator finishValidator = new FinalNodeValidator();
+        INodeValidator askAnotherOrderValidator = new OptionNodeValidator(["1", "2"]);
         INodeValidator askEmailValidator = new ValidationNodeValidator(emailValidator);
         INodeValidator askNameValidator = new ValidationNodeValidator(nameValidator);
+        INodeValidator askConfirmationValidator = new ConfirmationNodeValidator(["1", "2"]);
+        INodeValidator askQuantityValidator = new ValidationNodeValidator(quantityValidator);
+        INodeValidator askProductValidator = new ValidationNodeValidator(productValidator);
+        INodeValidator askCompanyCategoryValidator = new ValidationNodeValidator(companyCategoryValidator);
         INodeValidator startValidator = new OptionNodeValidator(["1", "2"]);
 
         IDomainEventFactory userSentNameDomainEventFactory = new UserSentNameDomainEventFactory();
         IDomainEventFactory userSentEmailDomainEventFactory = new UserSentEmailDomainEventFactory();
-        IDomainEventFactory userSentTypeDomainEventFactory = new UserSentTypeDomainEventFactory();
+        IDomainEventFactory customerDoesNotConfirmedRegistrationDomainEventFactory = new CustomerDoesNotConfirmedRegistrationDomainEventFactory();
+        IDomainEventFactory customerDoesNotConfirmedOrderDomainEventFactory = new CustomerDoesNotConfirmedOrderDomainEventFactory();
+        IDomainEventFactory customerSentQuantityDomainEventFactory = new CustomerSentQuantityDomainEventFactory();
+        IDomainEventFactory customerSentProductDomainEventFactory = new CustomerSentProductDomainEventFactory();
+        IDomainEventFactory customerSentCompanyCategoryDomainEventFactory = new CustomerSentCompanyCategoryDomainEventFactory();
         IDomainEventFactory userFinishedConversationDomainEventFactory = new UserFinishedConversationDomainEventFactory();
 
         var finish = new FinalNode
@@ -34,18 +44,32 @@ public class InitialRegistrationFlow
             DomainEventFactory = [userFinishedConversationDomainEventFactory],
         };
 
-        var askUserType = new OptionValidationNode
+        var askAnotherOrder = new OptionNode
         {
-            Id = "initial_ask_user_type",
-            NodeValidator = askUserTypeValidator,
+            Id = "customer_ask_another_order",
             Message =
                 """
-                Você deseja usar nosso sistema como cliente ou como fornecedor?
+                Pedido criado com sucesso!
 
-                1 - Cliente
-                2 - Fornecedor
+                Deseja criar outro pedido?
+                1 - Sim
+                2 - Não
                 """,
-            DomainEventFactory = [userSentTypeDomainEventFactory],
+            NodeValidator = askAnotherOrderValidator,
+        };
+
+        var askRegistrationConfirmation = new ConfirmationNode
+        {
+            Id = "customer_registration_ask_confirmation",
+            Message =
+                """
+                Nome e email passados estão corretos para que possamos colocar junto ao pedido ?
+
+                1 - Sim.
+                2 - Não, passar dados novamente.
+                """,
+            NodeValidator = askConfirmationValidator,
+            DomainEventFactory = [customerDoesNotConfirmedRegistrationDomainEventFactory]
         };
 
         var askEmail = new ValidationNode
@@ -64,14 +88,71 @@ public class InitialRegistrationFlow
             DomainEventFactory = [userSentNameDomainEventFactory],
         };
 
+        var askRegistration = new OptionNode
+        {
+            Id = "initial_ask_registration",
+            Message =
+                """
+                Para que possamos prosseguir com o envio do pedido aos fornecedores precisamos de seu nome completo e um email, deseja continuar ?
+
+                1 - Sim.
+                2 - Não, finalizar atendimento.
+                """,
+            NodeValidator = askConfirmationValidator,
+            DomainEventFactory = [userFinishedConversationDomainEventFactory],
+        };
+
+        var askOrderConfirmation = new ConfirmationNode
+        {
+            Id = "customer_order_ask_confirmation",
+            Message =
+                """
+                As informações passadas estão corretas para que possamos enviar o pedido aos fornecedores?
+
+                1 - Sim.
+                2 - Não, passar informações novamente.
+                """,
+            NodeValidator = askConfirmationValidator,
+            DomainEventFactory = [customerDoesNotConfirmedOrderDomainEventFactory]
+        };
+
+        var askQuantity = new ValidationNode
+        {
+            Id = "customer_ask_quantity",
+            Message = "Qual quantia deseja comprar?",
+            NodeValidator = askQuantityValidator,
+            DomainEventFactory = [customerSentQuantityDomainEventFactory],
+        };
+
+        var askProduct = new ValidationNode
+        {
+            Id = "customer_ask_product",
+            Message = "Qual produto deseja comprar?",
+            NodeValidator = askProductValidator,
+            DomainEventFactory = [customerSentProductDomainEventFactory],
+        };
+
+        var askCompanyCategory = new ValidationNode
+        {
+            Id = "customer_ask_company_category",
+            Message =
+                $"""
+                Qual categoria de produto deseja comprar?
+
+                {CompanyCategoryExtensions.PrintNames()}
+                """,
+            NodeValidator = askCompanyCategoryValidator,
+            DomainEventFactory = [customerSentCompanyCategoryDomainEventFactory],
+        };
+
         var start = new OptionNode
         {
             Id = "initial_start",
             Message =
                 """
-                Olá, bem-vindo! Vimos que é novo por aqui.
+                Olá, bem-vindo ao AnuncieCompre.
 
-                Deseja se registrar no AnuncieCompre para utilizar nosso sistema?
+                Deseja criar um pedido de compra ?
                 1 - Sim
                 2 - Não
                 """,
@@ -80,23 +161,47 @@ public class InitialRegistrationFlow
 
         //Transições de askUserType apenas para satisfazer o teste InitialRegistrationFlowTests
         //Ver depois uma forma de não ficar essa gambiarra
-        askUserType.Transitions["1"] = finish;
-        askUserType.Transitions["2"] = finish;
+        // askUserType.Transitions["1"] = finish;
+        // askUserType.Transitions["2"] = finish;
 
-        start.Transitions["1"] = askName;
+        start.Transitions["1"] = askCompanyCategory;
         start.Transitions["2"] = finish;
 
+        askCompanyCategory.Transitions["next"] = askProduct;
+
+        askProduct.Transitions["next"] = askQuantity;
+
+        askQuantity.Transitions["next"] = askOrderConfirmation;
+
+        askOrderConfirmation.Transitions["1"] = askRegistration;
+        askOrderConfirmation.Transitions["2"] = askCompanyCategory;
+
+        askRegistration.Transitions["1"] = askName;
+        askRegistration.Transitions["2"] = finish;
+
         askName.Transitions["next"] = askEmail;
-        askEmail.Transitions["next"] = askUserType;
+        askEmail.Transitions["next"] = askRegistrationConfirmation;
+
+        askRegistrationConfirmation.Transitions["1"] = askAnotherOrder;
+        askRegistrationConfirmation.Transitions["2"] = askName;
+
+        askAnotherOrder.Transitions["1"] = askCompanyCategory;
+        askAnotherOrder.Transitions["2"] = finish;
 
         finish.Transitions["next"] = start;
 
         return new Dictionary<string, IConversationNode>
         {
             { start.Id, start },
-            { askName.Id, askName },
-            { askEmail.Id, askEmail },
-            { askUserType.Id, askUserType },
+            {askCompanyCategory.Id, askCompanyCategory},
+            {askProduct.Id, askProduct},
+            {askQuantity.Id, askQuantity},
+            {askOrderConfirmation.Id, askOrderConfirmation},
+            {askRegistration.Id, askRegistration},
+            {askName.Id, askName},
+            {askEmail.Id, askEmail},
+            {askRegistrationConfirmation.Id, askRegistrationConfirmation},
+            {askAnotherOrder.Id, askAnotherOrder},
             { finish.Id, finish }
         }.AsReadOnly();
     }
