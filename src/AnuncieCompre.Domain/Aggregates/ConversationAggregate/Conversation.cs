00b5360427f36ea663159f17ebfb0c48263ab5ab
GustavoFMarcial <gfm211101@gmail.com>
2026-07-28 20:34:04 -0400

    corrigir iteração com usuário

--- src/AnuncieCompre.Domain/Aggregates/ConversationAggregate/Conversation.cs
+++ src/AnuncieCompre.Domain/Aggregates/ConversationAggregate/Conversation.cs
@@ -1,11 +1,9 @@
 using System.Collections.ObjectModel;
-using AnuncieCompre.Domain.Aggregates.ValueObjects;
 using AnuncieCompre.Domain.Aggregates.UserAggregate;
 using AnuncieCompre.Domain.Interfaces;
 using AnuncieCompre.Domain.Conversation.Nodes;
 using AnuncieCompre.Domain.Aggregates.MessageAggregate;
 using AnuncieCompre.Domain.Enums;
-using Microsoft.AspNetCore.Http.HttpResults;
 
 namespace AnuncieCompre.Domain.Aggregates.ConversationAggregate;
 
@@ -13,8 +11,7 @@ public class Conversation : BaseEntity
 {
     public Guid UserId { get; private set; }
     public User User { get; private set; } = default!;
-    public string AwaitingResponseNodeId { get; private set; } = default!;
-    public bool IsProcessing { get; private set; }
+    public string AwaitingResponseNodeId { get; private set; } = "start";
     public DateTime DateTimeLastMessage { get; private set; }
     public ConversationAttendant Attendant { get; private set; } = ConversationAttendant.Bot;
     public ConversationStatus Status { get; private set; } = ConversationStatus.JustCreated;
@@ -38,27 +35,11 @@ public class Conversation : BaseEntity
     {
         DateTimeLastMessage = DateTime.UtcNow;
 
-        if (IsProcessing)
-        {
-            return ["Só um momento, ainda estamos processando sua última mensagem"];
-        }
-
-        IsProcessing = true;
-
         if (Status == ConversationStatus.JustCreated)
         {
             Status = ConversationStatus.Open;
             AwaitingResponseNodeId = awaitingResponseNode.Id;
-            IsProcessing = false;
             return [awaitingResponseNode.Message];
-        }
-
-        if (awaitingResponseNode is FinalNode)
-        {
-            AwaitingResponseNodeId = awaitingResponseNode.Transitions["next"].Id;
-            Status = ConversationStatus.Closed;
-            IsProcessing = false;
-            return [awaitingResponseNode.Transitions["next"].Message];
         }
 
         NodeResult result = awaitingResponseNode.NodeValidator.Validate(awaitingResponseNode, message);
@@ -72,13 +53,12 @@ public class Conversation : BaseEntity
         }
 
         AwaitingResponseNodeId = result.NextStepId;
-        IsProcessing = false;
         return [result.Message];
     }
 
     public void Close()
     {
-        EndedAt = DateTime.Now;
+        EndedAt = DateTime.UtcNow;
         Status = ConversationStatus.Closed;
     }
 }
