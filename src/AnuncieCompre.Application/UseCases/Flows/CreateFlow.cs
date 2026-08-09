06dd35d997fa74473d61a7aae5f10b6eadf627de
GustavoFMarcial <gfm211101@gmail.com>
2026-08-09 11:43:29 -0400

    atualiza models Node e Flow

--- src/AnuncieCompre.Application/UseCases/Flows/CreateFlow.cs
+++ src/AnuncieCompre.Application/UseCases/Flows/CreateFlow.cs
@@ -1,6 +1,7 @@
 using AnuncieCompre.Application.Interfaces;
 using AnuncieCompre.Domain.Aggregates.FlowAggregate;
 using AnuncieCompre.Domain.Aggregates.ValueObjects;
+using AnuncieCompre.Domain.Common;
 using AnuncieCompre.Domain.DTO;
 using AnuncieCompre.Infra.Repositories;
 
@@ -11,10 +12,10 @@ public class CreateFlow(IFlowRepository _flowRepository, IUnitOfWork _unitOfWork
     private readonly IFlowRepository flowRepository = _flowRepository;
     private readonly IUnitOfWork unitOfWork = _unitOfWork;
 
-    public async Task<Flow> Handle(CreateFlowRequest request)
+    public async Task<Result<Flow>> Handle(CreateFlowRequest request)
     {
-        Flow flow = Flow.Create(Name.Create(request.Name).Value, request.Description, request.Status);
-        flowRepository.Add(flow);
+        Result<Flow> flow = Flow.Create(request.Name, request.Description, request.Status);
+        flowRepository.Add(flow.Value);
         await unitOfWork.SaveChangesAsync();
 
         return flow;
