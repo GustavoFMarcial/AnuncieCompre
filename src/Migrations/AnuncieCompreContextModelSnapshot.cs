e59a311458ad0ba47075f61e49c5d09ce39b8353
GustavoFMarcial <gfm211101@gmail.com>
2026-09-20 14:17:59 -0400

    fix

--- src/Migrations/AnuncieCompreContextModelSnapshot.cs
+++ src/Migrations/AnuncieCompreContextModelSnapshot.cs
@@ -53,43 +53,6 @@ namespace AnuncieCompre.Migrations
                     b.Property<DateTime>("UpdateAt")
                         .HasColumnType("timestamp with time zone");
 
-                    b.ComplexProperty(typeof(Dictionary<string, object>), "Customer", "AnuncieCompre.Domain.Aggregates.ConversationAggregate.Conversation.Customer#Customer", b1 =>
-                        {
-                            b1.IsRequired();
-
-                            b1.Property<DateTime>("CreatedAt")
-                                .HasColumnType("timestamp with time zone");
-
-                            b1.Property<Guid>("Id")
-                                .HasColumnType("uuid");
-
-                            b1.Property<DateTime>("UpdateAt")
-                                .HasColumnType("timestamp with time zone");
-
-                            b1.ComplexProperty(typeof(Dictionary<string, object>), "Email", "AnuncieCompre.Domain.Aggregates.ConversationAggregate.Conversation.Customer#Customer.Email#Email", b2 =>
-                                {
-                                    b2.Property<string>("Value")
-                                        .IsRequired()
-                                        .HasColumnType("text");
-                                });
-
-                            b1.ComplexProperty(typeof(Dictionary<string, object>), "Name", "AnuncieCompre.Domain.Aggregates.ConversationAggregate.Conversation.Customer#Customer.Name#Name", b2 =>
-                                {
-                                    b2.Property<string>("Value")
-                                        .IsRequired()
-                                        .HasColumnType("text");
-                                });
-
-                            b1.ComplexProperty(typeof(Dictionary<string, object>), "Phone", "AnuncieCompre.Domain.Aggregates.ConversationAggregate.Conversation.Customer#Customer.Phone#Phone", b2 =>
-                                {
-                                    b2.IsRequired();
-
-                                    b2.Property<string>("Value")
-                                        .IsRequired()
-                                        .HasColumnType("text");
-                                });
-                        });
-
                     b.HasKey("Id");
 
                     b.HasIndex("CustomerId");
